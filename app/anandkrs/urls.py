2ba55859bc48ecbb13debee1bb17f1efd07f4541
Anand Singh <email@anandkrs.com>
2016-07-27 05:12:47 -0500

    Updated with the ibasic view.

--- app/anandkrs/urls.py
+++ app/anandkrs/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+# from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url 
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+
+    # Blog URL
+    url(r'^.*$', include('blogengine.urls')),
 ]
+
+#urlpatterns = [
+#    url(r'^admin/', include(admin.site.urls)),
+#]
+
