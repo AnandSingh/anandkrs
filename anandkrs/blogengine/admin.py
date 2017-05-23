21a250dc0a3d1d5c7f93531a382b2c26312c2f69
Anand Singh <email@anandkrs.com>
2017-05-22 23:02:21 -0700

    Implement post pages

--- anandkrs/blogengine/admin.py
+++ anandkrs/blogengine/admin.py
@@ -1,5 +1,8 @@
 from blogengine.models import Post
 from django.contrib import admin
 
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
--- anandkrs/blogengine/tests.py
+++ anandkrs/blogengine/tests.py
@@ -1,20 +1,25 @@
 from django.test import TestCase, LiveServerTestCase, Client
 from django.utils import timezone
 from blogengine.models import Post
-import markdown
+import markdown2
 
 # Create your tests here.
 # Create your tests here.
 class PostTest(TestCase):
     def test_create_post(self):
+
         # Create the post
         post = Post()
+
         # Set the attributes
         post.title = 'My first post'
         post.text = 'This is my first blog post'
+        post.slug = 'post'
         post.pub_date = timezone.now()
+
         # Save it
         post.save()
+
         # Check we can find it
         all_posts = Post.objects.all()
         self.assertEquals(len(all_posts), 1)
@@ -77,7 +82,8 @@ class AdminTest(LiveServerTestCase):
             'title': 'My first post',
             'text': 'This is my first post',
             'pub_date_0': '2013-12-28',
-            'pub_date_1': '22:00:04'
+            'pub_date_1': '22:00:04',
+            'slug': 'my-first-post'
         },
         follow=True
         )
@@ -96,19 +102,22 @@ class AdminTest(LiveServerTestCase):
         post.pub_date = timezone.now()
         post.save()
         # Log in
-        self.client.login(username='bobsmith', password="password")
+        self.client.login(username='anand', password="Qwepoi!123")
         # Edit the post
         response = self.client.post('/admin/blogengine/post/1/', {
             'title': 'My second post',
             'text': 'This is my second blog post',
             'pub_date_0': '2013-12-28',
-            'pub_date_1': '22:00:04'
+            'pub_date_1': '22:00:04',
+            'slug': 'my-second-post'
         },
         follow=True
         )
         self.assertEquals(response.status_code, 200)
+
         # Check changed successfully
         self.assertTrue('changed successfully' in response.content)
+
         # Check post amended
         all_posts = Post.objects.all()
         self.assertEquals(len(all_posts), 1)
@@ -123,18 +132,23 @@ class AdminTest(LiveServerTestCase):
         post.text = 'This is my first blog post'
         post.pub_date = timezone.now()
         post.save()
+
         # Check new post saved
         all_posts = Post.objects.all()
         self.assertEquals(len(all_posts), 1)
+
         # Log in
         self.client.login(username='bobsmith', password="password")
+
         # Delete the post
         response = self.client.post('/admin/blogengine/post/1/delete/', {
             'post': 'yes'
         }, follow=True)
         self.assertEquals(response.status_code, 200)
+
         # Check deleted successfully
         self.assertTrue('deleted successfully' in response.content)
+
         # Check post amended
         all_posts = Post.objects.all()
         self.assertEquals(len(all_posts), 0)
@@ -165,3 +179,33 @@ class PostViewTest(LiveServerTestCase):
         self.assertTrue(str(post.pub_date.day) in response.content)
         # Check the link is marked up properly
         self.assertTrue('<a href="http://127.0.0.1:8000/">my first blog post</a>' in response.content)
+
+    def test_post_page(self):
+        # Create the post
+        post = Post()
+        post.title = 'My first post'
+        post.text = 'This is [my first blog post](http://127.0.0.1:8000/)'
+        post.slug = 'my-first-post'
+        post.pub_date = timezone.now()
+        post.save()
+        # Check new post saved
+        all_posts = Post.objects.all()
+        self.assertEquals(len(all_posts), 1)
+        only_post = all_posts[0]
+        self.assertEquals(only_post, post)
+        # Get the post URL
+        post_url = only_post.get_absolute_url()
+        # Fetch the post
+        response = self.client.get(post_url)
+        self.assertEquals(response.status_code, 200)
+        # Check the post title is in the response
+        self.assertTrue(post.title in response.content)
+        # Check the post text is in the response
+        self.assertTrue(markdown.markdown(post.text) in response.content)
+        # Check the post date is in the response
+        self.assertTrue(str(post.pub_date.year) in response.content)
+        self.assertTrue(post.pub_date.strftime('%b') in response.content)
+        self.assertTrue(str(post.pub_date.day) in response.content)
+        # Check the link is marked up properly
+        self.assertTrue('<a href="http://127.0.0.1:8000/">my first blog post</a>' in response.content)
+
