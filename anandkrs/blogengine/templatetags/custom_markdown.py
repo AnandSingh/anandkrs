37a1d356ec1b74c82fe8ec298ba84970ca195468
Anand Singh <email@anandkrs.com>
2017-05-23 11:44:50 -0700

    Now use Markdown2 to allow for syntax highlighting

--- anandkrs/blogengine/templatetags/custom_markdown.py
+++ anandkrs/blogengine/templatetags/custom_markdown.py
@@ -1,8 +1,7 @@
 import markdown2
-
 from django import template
 from django.template.defaultfilters import stringfilter
-from django.utils.encoding import force_str
+from django.utils.encoding import force_unicode
 from django.utils.safestring import mark_safe
 
 register = template.Library()
@@ -11,5 +10,5 @@ register = template.Library()
 @stringfilter
 def custom_markdown(value):
     extras = ["fenced-code-blocks"]
-    return mark_safe(markdown2.markdown(force_str(value),
+    return mark_safe(markdown2.markdown(force_unicode(value),
                                        extras = extras))
