2870759ec550f78710e369c1ed86ed866cb8321a
Anand Singh <email@anandkrs.com>
2017-05-23 09:37:27 -0700

    Implement categories

--- anandkrs/blogengine/admin.py
+++ anandkrs/blogengine/admin.py
@@ -1,8 +1,10 @@
-from blogengine.models import Post
+from blogengine.models import Post, Category
 from django.contrib import admin
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
+
 # Register your models here.
+admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
