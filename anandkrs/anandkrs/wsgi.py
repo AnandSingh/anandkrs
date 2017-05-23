82c4e8cfbcac0d938ae9b24abf1d2d9ccca7a909
Anand Singh <email@anandkrs.com>
2017-05-23 11:45:31 -0700

    Configured app for deployment on Heroku - 2

--- anandkrs/anandkrs/wsgi.py
+++ anandkrs/anandkrs/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_tutorial_blog_ng.settings")
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "anandkrs.settings")
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
