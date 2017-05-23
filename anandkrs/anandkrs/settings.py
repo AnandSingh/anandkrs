3de6765075041a57dcb4b291b9d2e8d1d7095394
Anand Singh <email@anandkrs.com>
2017-05-23 11:11:03 -0700

    Added django_jenkins for code coverage

--- anandkrs/anandkrs/settings.py
+++ anandkrs/anandkrs/settings.py
@@ -131,6 +131,5 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 INSTALLED_APPS += ('django_jenkins',)
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pylint',
-    'django_jenkins.tasks.with_coverage',
 )
 PROJECT_APPS = ['blogengine']
